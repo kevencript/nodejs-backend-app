32fbcf2299c1193ff67089887db34dd648fb2901
Gabriel Braga <kevencript@gmail.com>
2020-01-15 00:25:17 -0300

    OK

--- middleware/auth.js
+++ middleware/auth.js
@@ -30,6 +30,6 @@ module.exports = function(req, res, next) {
     req.user = decoded.user;
     next();
   } catch (err) {
-    res.status(401).json({ errorMessage: "O token não é válido" });
+    return res.status(401).json({ errorMessage: "O token não é válido" });
   }
 };
