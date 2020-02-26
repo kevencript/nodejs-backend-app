cdf9b55b63a1b915a55eccdfed121bd31d617d2c
Gabriel Braga <kevencript@gmail.com>
2020-02-26 15:26:03 -0300

    Adicionada a rota de "Pre-agendamento"

--- routes/api/agendamentos/controller.js
+++ routes/api/agendamentos/controller.js
@@ -13,7 +13,7 @@ const {
   capturarVenda
 } = require("../../../utilitarios/cielo");
 const { check, validationResult } = require("express-validator");
-
+const moment = require("moment");
 // models
 const {
   sys_users,
@@ -92,7 +92,6 @@ exports.cartao_credito = async (req, res) => {
     // Verificando se o PIN está expirado
     const minutosDeDiferença = duration.asMinutes();
 
-    // Ois
     return res.json(minutosDeDiferença);
 
     // Desconstruindo informações do Body
@@ -189,6 +188,199 @@ exports.cartao_credito = async (req, res) => {
       errors: [
         {
           msg: "Erro ao realizar agendamento",
+          callback: error.message
+        }
+      ]
+    });
+  }
+};
+
+// @type     Middleware de validação dos campos
+// @route    POST /api/agendamentos/pre-agendar
+exports.validatorPreAgendar = [
+  check("id_estabelecimento")
+    .not()
+    .isEmpty()
+    .withMessage("Por favor, identificar o estabelecimento")
+    .not()
+    .isString()
+    .withMessage(
+      "O identificador do estabelecimento deve ser um valor inteiro"
+    ),
+  check("id_estabelecimento_servico")
+    .not()
+    .isEmpty()
+    .withMessage("Por favor, identificar o serviço")
+    .not()
+    .isString()
+    .withMessage("O identificador do serviço deve ser um valor inteiro"),
+  check("id_funcionarioagendamento")
+    .not()
+    .isEmpty()
+    .withMessage("Por favor, identificar o funcionário")
+    .not()
+    .isString()
+    .withMessage("O identificador do funcionário deve ser um valor inteiro"),
+  check("id_horario")
+    .not()
+    .isEmpty()
+    .withMessage("Por favor, identificar o identificador do horário")
+    .not()
+    .isString()
+    .withMessage("O identificador do horário deve ser um valor inteiro")
+];
+
+// @route    POST /api/agendamentos/pre-agendar
+// @desc     Realizar um pré-agendamento
+exports.pre_agendar = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  try {
+    // Retornando dados do usuário logado
+    const user = await sys_users.findOne({
+      where: {
+        uuid_sysusers: req.user.id
+      }
+    });
+
+    const {
+      id_estabelecimento,
+      id_estabelecimento_servico,
+      id_funcionarioagendamento,
+      id_horario
+    } = req.body;
+
+    // Configuração de dados Fixos
+    const id_statusagendamento = 1; // status "cadastrado"
+    const id_usuariostatus = 1; // status inicial
+    const status_funcionario = 1; // 1=agendado, 0=cancelado, 2=expirado
+    const timestamp = moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
+    const id_cliente = parseInt(user.id_sysusers);
+
+    // Validando se o horário está ocupado
+    const isOcupado = await sequelize.query(
+      `
+        SELECT
+            dthoraregistro
+        FROM
+                age_funcionarios_servicos S
+        INNER JOIN
+                est_horarios H
+        ON
+                H.id_estabelecimento = S.id_estabelecimento
+        AND     H.id_horario         = S.id_horario
+        WHERE
+                h.id_estabelecimento = :id_estabelecimento --parametro
+        AND		id_funcionario = :id_funcionarioagendamento 
+        AND		H.datatrabalho = H.datatrabalho
+        AND 	H.id_horario = :id_horario;
+    `,
+      {
+        replacements: {
+          id_horario,
+          id_estabelecimento,
+          id_funcionarioagendamento
+        },
+        type: sequelize.QueryTypes.SELECT
+      }
+    );
+
+    if (isOcupado[0]) {
+      throw new Error("Horário ocupado");
+    }
+
+    // Inserindo pre-agendamento
+    const insertPreAgendamento = await sequelize.query(
+      `
+          INSERT INTO
+              public.age_agendamentos_servicos
+              (
+                      id_estabelecimento        ,
+                      id_estabelecimento_servico,
+                      id_statusagendamento        ,
+                      datahorastatus              ,
+                      idusuariostatus           ,
+                      id_cliente,
+                      id_horario,
+                      id_funcionarioagendamento
+              )
+        SELECT
+              :id_estabelecimento             id_estabelecimento        ,
+              :id_estabelecimento_servico     id_estabelecimento_servico,	--parametro = id do servico selecionado
+              :id_statusagendamento           id_statusagendamento       ,	-- fixo - status agendamento cadastrado
+              TIMESTAMP :timestamp            datahorastatus              ,
+              :id_usuariostatus               idusuariostatus           ,
+              :id_cliente                     id_cliente, --parametro
+              :id_horario                     id_horario,
+              :id_funcionarioagendamento      --parametro do funcionario que realizara o servico do agendamento
+
+        RETURNING id_agendamento_servico;
+    
+    `,
+      {
+        replacements: {
+          id_estabelecimento,
+          id_estabelecimento_servico,
+          id_statusagendamento,
+          timestamp,
+          id_usuariostatus,
+          id_cliente,
+          id_horario,
+          id_funcionarioagendamento
+        },
+        type: sequelize.QueryTypes.INSERT
+      }
+    );
+
+    // Inserindo na tabela do funcionário
+    const insertFuncionario = await sequelize.query(
+      `
+      INSERT INTO
+        public.age_funcionarios_servicos
+      (
+        id_cliente,
+        status,
+        id_funcionario,
+        id_estabelecimento,
+        dthoraregistro,
+        id_horario
+      )
+      VALUES (
+
+        :id_cliente ,
+        :status_funcionario,                     
+        :id_funcionarioagendamento,              
+        :id_estabelecimento,                     
+        :timestamp,                              
+        :id_horario                              
+      )`,
+      {
+        replacements: {
+          id_cliente,
+          status_funcionario,
+          id_funcionarioagendamento,
+          id_estabelecimento,
+          timestamp,
+          id_horario
+        },
+        type: sequelize.QueryTypes.INSERT
+      }
+    );
+
+    const id_agendamento_servico = parseInt(
+      insertPreAgendamento[0][0].id_agendamento_servico
+    );
+
+    res.json({ id_agendamento_servico });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      errors: [
+        {
+          msg: "Erro ao realizar pre-agendamento",
           callback: error.message
         }
       ]
--- routes/api/agendamentos/index.js
+++ routes/api/agendamentos/index.js
@@ -9,11 +9,21 @@ const router = express.Router();
 const Auth = require("../../../middleware/auth");
 
 // controller import
-const { cartao_credito, validatorCartaoCredito } = require("./controller");
+const {
+  cartao_credito,
+  validatorCartaoCredito,
+  pre_agendar,
+  validatorPreAgendar
+} = require("./controller");
 
 // @route    POST /api/agendamentos/cartao-credito
 // @desc     Realizar um agendamento utilizando o cartão de crédito como forma de pagamento
 // @acess    Private
 router.post("/cartao-credito", Auth, validatorCartaoCredito, cartao_credito);
+
+// @route    POST /api/agendamentos/pre-agendar
+// @desc     Realizar um pré-agendamento
+// @acess    Private
+router.post("/pre-agendar", Auth, validatorPreAgendar, pre_agendar);
 
 module.exports = router;
